33c093beec374757e5da58787fddbf0b77ebdbe6
samsoffes <sam@samsoff.es>
2009-09-28 12:51:07 -0500

    Another minor tweak

--- TWToolkit/Vendor/Reachability/Reachability.h
+++ TWToolkit/Vendor/Reachability/Reachability.h
@@ -53,6 +53,7 @@ typedef enum {
 	ReachableViaWiFi,
 	ReachableViaWWAN
 } NetworkStatus;
+
 #define kReachabilityChangedNotification @"kNetworkReachabilityChangedNotification"
 
 @interface Reachability: NSObject
@@ -62,25 +63,25 @@ typedef enum {
 }
 
 //reachabilityWithHostName- Use to check the reachability of a particular host name. 
-+ (Reachability *) reachabilityWithHostName: (NSString*) hostName;
++ (Reachability *)reachabilityWithHostName: (NSString*) hostName;
 
 //reachabilityWithAddress- Use to check the reachability of a particular IP address. 
-+ (Reachability *) reachabilityWithAddress: (const struct sockaddr_in*) hostAddress;
++ (Reachability *)reachabilityWithAddress: (const struct sockaddr_in*) hostAddress;
 
 //reachabilityForInternetConnection- checks whether the default route is available.  
 //  Should be used by applications that do not connect to a particular host
-+ (Reachability *) reachabilityForInternetConnection;
++ (Reachability *)reachabilityForInternetConnection;
 
 //reachabilityForLocalWiFi- checks whether a local wifi connection is available.
-+ (Reachability *) reachabilityForLocalWiFi;
++ (Reachability *)reachabilityForLocalWiFi;
 
 //Start listening for reachability notifications on the current run loop
 - (BOOL) startNotifer;
 - (void) stopNotifer;
 
-- (NetworkStatus) currentReachabilityStatus;
+- (NetworkStatus)currentReachabilityStatus;
 //WWAN may be available, but not active until a connection has been established.
 //WiFi may require a connection for VPN on Demand.
-- (BOOL) connectionRequired;
+- (BOOL)connectionRequired;
 
 @end
