febab2e310465cb5fa852bed5f6177c264e0a22f
samsoffes <sam@samsoff.es>
2009-09-29 13:22:24 -0500

    Started on switch cell

---
+++ TWToolkit/TWSwitchTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  TWSwitchTableViewCell.h
+//  TWToolkit
+//
+//  Created by Sam Soffes on 9/29/09.
+//  Copyright 2009 Tasteful Works, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWSwitchTableViewCell : UITableViewCell {
+
+}
+
+@end
---
+++ TWToolkit/TWSwitchTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  TWSwitchTableViewCell.m
+//  TWToolkit
+//
+//  Created by Sam Soffes on 9/29/09.
+//  Copyright 2009 Tasteful Works, Inc. All rights reserved.
+//
+
+#import "TWSwitchTableViewCell.h"
+
+@implementation TWSwitchTableViewCell
+
+@end
