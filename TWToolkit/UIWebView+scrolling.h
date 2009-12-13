9a62d3c10f7ed3ac228be4beab8999675f6307b0
Sam Soffes <sam@samsoff.es>
2009-12-13 13:21:41 -0600

    Added parts of UIWebView+scrolling back

---
+++ TWToolkit/UIWebView+scrolling.h
@@ -0,0 +1,15 @@
+//
+//  UIWebView+scrolling.h
+//  TWToolkit
+//
+//  Created by Sam Soffes on 6/22/09.
+//  Copyright 2009 Tasteful Works, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (scrolling)
+
+- (UIScrollView *)scroller;
+
+@end
