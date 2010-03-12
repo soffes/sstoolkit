47cb041f7bcbdc98fc1a47f33cc0bc82997dde56
Sam Soffes <sam@samsoff.es>
2010-03-11 20:47:18 -0600

    Commented message view controller

--- TWToolkit/TWMessagesViewController.h
+++ TWToolkit/TWMessagesViewController.h
@@ -2,6 +2,11 @@
 //  TWMessagesViewController.h
 //  Messages
 //
+//	This is an abstract class for displaying a UI similar
+//	to Apple's SMS application. A subclass should
+//	override the messageStyleForRowAtIndexPath: and
+//	textForRowAtIndexPath: to customize this class.
+//
 //  Created by Sam Soffes on 3/10/10.
 //  Copyright 2010 Tasteful Works. All rights reserved.
 //
