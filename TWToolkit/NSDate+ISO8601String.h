391253e6a57b6b6aec1059859ef7da822e535661
Sam Soffes <sam@samsoff.es>
2010-07-07 09:48:54 -0500

    Improved NSDate ISO8601 category

--- TWToolkit/NSDate+ISO8601String.h
+++ TWToolkit/NSDate+ISO8601String.h
@@ -1,13 +1,14 @@
 //
-//  NSDate+dateFromISO8601String.h
+//  NSDate+ISO8601String.h
 //  TWToolkit
 //
 //  Created by Sam Soffes on 5/26/10.
 //  Copyright 2010 Tasteful Works, Inc. All rights reserved.
 //
 
-@interface NSDate (dateFromISO8601String)
+@interface NSDate (ISO8601String)
 
 + (NSDate *)dateFromISO8601String:(NSString *)string;
+- (NSString *)ISO8601String;
 
 @end
--- TWToolkit/NSDate+ISO8601String.m
+++ TWToolkit/NSDate+ISO8601String.m
@@ -1,12 +1,12 @@
 //
-//  NSDate+dateFromISO8601String.m
+//  NSDate+ISO8601String.m
 //  TWToolkit
 //
 //  Created by Sam Soffes on 5/26/10.
 //  Copyright 2010 Tasteful Works, Inc. All rights reserved.
 //
 
-#import "NSDate+dateFromISO8601String.h"
+#import "NSDate+ISO8601String.h"
 
 @implementation NSDate (dateFromISO8601String)
 
@@ -14,6 +14,13 @@
 	NSDateFormatter* dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
 	[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
     return [dateFormatter dateFromString:string];
+}
+
+
+- (NSString *)ISO8601String {
+	NSDateFormatter* dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
+	[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+	return [dateFormatter stringFromDate:self];
 }
 
 @end
