0dea880ee120a389e2db602a1c59667e53cda20b
Hector Ramos <hector.ramos@gmail.com>
2011-01-30 19:22:56 -0400

    Add documentation to SSBadgeView

--- SSToolkit/SSBadgeView.h
+++ SSToolkit/SSBadgeView.h
@@ -12,6 +12,21 @@ typedef enum {
 	SSBadgeViewAlignmentRight = UITextAlignmentRight
 } SSBadgeViewAlignment;
 
+/**
+ @brief Badge view.
+ 
+ Provides the accesory view for a SSBadgeTableViewCell, available through the 
+ badgeView property of the same.
+ 
+ Acts very much like the badges in Mail.app, with the key difference being that 
+ Apple uses images and SSBadgeView is rendered with CoreGraphics for improved 
+ scrolling performance.
+ 
+ Not limited to numbers, strings should still be short enough to fit within the 
+ accesoryView bounds. 
+ 
+ */
+
 @interface SSBadgeView : UIView {
 
 	NSString *_text;
@@ -29,14 +44,63 @@ typedef enum {
 	BOOL _hasDrawn;
 }
 
+/**
+ @brief Display value.
+
+ Default is 0.
+ */
 @property (nonatomic, copy) NSString *text;
+
+/**
+ @brief The color of the badge display value.
+ 
+ Default is white.
+ */
 @property (nonatomic, retain) UIColor *textColor;
+
+/**
+ @brief The color of the badge display value while its cell is highlighted.
+ 
+ Default is blue (matches UITableViewCell's default highlight background color).
+ */
 @property (nonatomic, retain) UIColor *highlightedTextColor;
+
+/**
+ @brief Read-only value for cell's highlighted status.
+ */
 @property (nonatomic, retain) UIFont *font;
+
+/**
+ @brief The badge's background color.
+ 
+ Default is grayish blue.
+ */
 @property (nonatomic, retain) UIColor *badgeColor;
+
+/**
+ @brief The badge's background color while its cell is highlighted.
+ 
+ Default is white.
+ */
 @property (nonatomic, retain) UIColor *highlightedBadgeColor;
+
+/**
+ @brief The corner radius used when rendering the badge's outline.
+ 
+ Default is 10.
+ */
 @property (nonatomic, assign) CGFloat cornerRadius;
+
+/**
+ @brief The badge's horizontal alignment within the accesoryView frame.
+ 
+ Default is <code>SSBadgeViewAlignmentRight</code>.
+ */
 @property (nonatomic, assign) SSBadgeViewAlignment badgeAlignment;
+
+/**
+ @brief Parent cell's highlighted status. (read-only)
+ */
 @property (nonatomic, assign, getter=isHighlighted) BOOL highlighted;
 
 @end
