509e57a9f383fd8c5999d0985014c3df504a4075
Sam Soffes <sam@samsoff.es>
2011-03-25 15:06:21 -0700

    Fixed HUD placement issues

--- SSToolkit/SSHUDWindow.m
+++ SSToolkit/SSHUDWindow.m
@@ -35,10 +35,6 @@
 	UIImage *image = [UIImage imageNamed:imageName bundle:kSSToolkitBundleName];
 	
 	CGSize screenSize = [[UIScreen mainScreen] bounds].size;
-	if (UIDeviceOrientationIsLandscape([[UIDevice currentDevice] orientation])) {
-		screenSize = CGSizeMake(screenSize.height, screenSize.width);
-	}
-	
 	[image drawInRect:CGRectMake(roundf((screenSize.width - image.size.width) / 2.0f), 
 								 roundf((screenSize.height - image.size.height) / 2.0f), 
 								 image.size.width, image.size.height)];	
