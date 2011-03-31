d643f7f0db7a0557821a45dff5aef7549b8cd08e
Sam Soffes <sam@samsoff.es>
2011-03-31 16:30:01 -0700

    Added clearButtonEdgeInsets to SSTextField

--- SSToolkit/SSTextField.h
+++ SSToolkit/SSTextField.h
@@ -14,17 +14,27 @@
 @private
 	
 	UIEdgeInsets _textEdgeInsets;
+	UIEdgeInsets _clearButtonEdgeInsets;
 }
 
 /**
  @brief The inset or outset margins for the edges of the text content drawing rectangle.
  
- Use this property to resize and reposition the effective drawing rectangle for the text content.
- You can specify a different value for each of the four insets (top, left, bottom, right). A positive
- value shrinks, or insets, that edge—moving it closer to the center of the button. A negative value
- expands, or outsets, that edge. Use the UIEdgeInsetsMake function to construct a value for this
- property. The default value is UIEdgeInsetsZero.
+ Use this property to resize and reposition the effective drawing rectangle for the text content. You can specify a
+ different value for each of the four insets (top, left, bottom, right). A positive value shrinks, or insets, that
+ edge—moving it closer to the center of the button. A negative value expands, or outsets, that edge. Use the
+ UIEdgeInsetsMake function to construct a value for this property. The default value is UIEdgeInsetsZero.
  */
 @property (nonatomic, assign) UIEdgeInsets textEdgeInsets;
+
+/**
+ @brief The inset or outset margins for the edges of the clear button drawing rectangle.
+ 
+ Use this property to resize and reposition the effective drawing rectangle for the clear button content. You can
+ specify a different value for each of the four insets (top, left, bottom, right), but only the top and right insets are
+ respected. A positive value will move the clear button farther away from the top right corner. Use the UIEdgeInsetsMake
+ function to construct a value for this property. The default value is UIEdgeInsetsZero.
+ */
+@property (nonatomic, assign) UIEdgeInsets clearButtonEdgeInsets;
 
 @end
--- SSToolkit/SSTextField.m
+++ SSToolkit/SSTextField.m
@@ -7,16 +7,19 @@
 //
 
 #import "SSTextField.h"
+#import "SSDrawingUtilities.h"
 
 @implementation SSTextField
 
 @synthesize textEdgeInsets = _textEdgeInsets;
+@synthesize clearButtonEdgeInsets = _clearButtonEdgeInsets;
 
 #pragma mark UIView
 
 - (id)initWithFrame:(CGRect)frame {
     if ((self = [super initWithFrame:frame])) {
         _textEdgeInsets = UIEdgeInsetsZero;
+		_clearButtonEdgeInsets = UIEdgeInsetsZero;
     }
     return self;
 }
@@ -25,12 +28,19 @@
 #pragma mark UITextField
 
 - (CGRect)textRectForBounds:(CGRect)bounds {
-	return UIEdgeInsetsInsetRect(bounds, _textEdgeInsets);
+	return UIEdgeInsetsInsetRect([super textRectForBounds:bounds], _textEdgeInsets);
 }
 
 
 - (CGRect)editingRectForBounds:(CGRect)bounds {
 	return [self textRectForBounds:bounds];
+}
+
+
+- (CGRect)clearButtonRectForBounds:(CGRect)bounds {
+	CGRect rect = [super clearButtonRectForBounds:bounds];
+	rect = CGRectSetY(rect, rect.origin.y + _clearButtonEdgeInsets.top);
+	return CGRectSetX(rect, rect.origin.x + _clearButtonEdgeInsets.right);
 }
 
 @end
