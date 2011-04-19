b771775567f14d93f0dd2270662fda553db73bca
Alexandre Laborie <alaborie@gmail.com>
2011-04-19 16:40:49 -0700

    Added a method to know if a screen is a retina display.

--- SSToolkit/UIScreen+SSToolkitAdditions.m
+++ SSToolkit/UIScreen+SSToolkitAdditions.m
@@ -17,14 +17,24 @@
 
 - (CGRect)boundsForOrientation:(UIInterfaceOrientation)orientation {
 	CGRect bounds = [self bounds];
-	
+
 	if (UIInterfaceOrientationIsLandscape(orientation)) {
 		CGFloat buffer = bounds.size.width;
-		
+
 		bounds.size.width = bounds.size.height;
 		bounds.size.height = buffer;
 	}
 	return bounds;
+}
+
+- (BOOL)isRetinaDisplay {
+	static dispatch_once_t predicate;
+	static BOOL answer;
+
+	dispatch_once(&predicate, ^(){
+		answer = ([self respondsToSelector:@selector(scale)] && [self scale] == 2);
+	});
+	return answer;
 }
 
 @end
