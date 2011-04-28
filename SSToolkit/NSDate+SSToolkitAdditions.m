d2b195a2efa7a15debe9d119e21123a03f36059e
Sam Soffes <sam@samsoff.es>
2011-04-27 17:41:52 -0700

    briefTimeAgoInWords is now briefer

--- SSToolkit/NSDate+SSToolkitAdditions.m
+++ SSToolkit/NSDate+SSToolkitAdditions.m
@@ -86,26 +86,16 @@
 	NSTimeInterval intervalInSeconds = fabs([self timeIntervalSinceNow]);
 	NSTimeInterval intervalInMinutes = round(intervalInSeconds / 60.0f);
 	
-	if (intervalInMinutes >= 0 && intervalInMinutes <= 1) {
-		return @"1 min";		
-	} else if (intervalInMinutes >= 2 && intervalInMinutes < 60) {
-		return [NSString stringWithFormat:@"%.0f mins", intervalInMinutes];
-	} else if (intervalInMinutes >= 60 && intervalInMinutes < 61) {
-		return @"1 hour";
-	} else if (intervalInMinutes > 60 && intervalInMinutes < 1440) {
-		return [NSString stringWithFormat:@"%.0f hours", round(intervalInMinutes/60.0f)];
-	} else if (intervalInMinutes >= 1440 && intervalInMinutes <= 2879) {
-		return @"1 day";
-	} else if (intervalInMinutes >= 2880 && intervalInMinutes <= 43199) {
-		return [NSString stringWithFormat:@"%.0f days", round(intervalInMinutes/1440.0f)];
-	} else if (intervalInMinutes >= 43200 && intervalInMinutes <= 86399) {
-		return @"1 month";
-	} else if (intervalInMinutes >= 86400 && intervalInMinutes <= 525599) {
-		return [NSString stringWithFormat:@"%.0f months", round(intervalInMinutes/43200.0f)];
-	} else if (intervalInMinutes >= 525600 && intervalInMinutes <= 1051199) {
-		return @"1 year";
-	} else {
-		return [NSString stringWithFormat:@"%.0f years", round(intervalInMinutes/525600.0f)];
+	if (intervalInMinutes >= 0 && intervalInMinutes < 60) {
+		return [NSString stringWithFormat:@"%.0fm", intervalInMinutes];
+	} else if (intervalInMinutes >= 60 && intervalInMinutes < 1440) {
+		return [NSString stringWithFormat:@"%.0fh", round(intervalInMinutes/60.0f)];
+	} else if (intervalInMinutes >= 1440 && intervalInMinutes <= 43199) {
+		return [NSString stringWithFormat:@"%.0fd", round(intervalInMinutes/1440.0f)];
+	} else if (intervalInMinutes >= 43200 && intervalInMinutes <= 525599) {
+		return [NSString stringWithFormat:@"%.0fmo", round(intervalInMinutes/43200.0f)];
+	} else if (intervalInMinutes >= 525600) {
+		return [NSString stringWithFormat:@"%.0fy", round(intervalInMinutes/525600.0f)];
 	}
 	return nil;
 }
