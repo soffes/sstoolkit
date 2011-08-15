9beaa2a791d622303270ef987de2753d88b89e28
Sam Soffes <sam@samsoff.es>
2011-08-15 10:15:01 -0700

    Fixed typeo in UIImage category

--- SSToolkit/UIImage+SSToolkitAdditions.m
+++ SSToolkit/UIImage+SSToolkitAdditions.m
@@ -23,11 +23,11 @@
 
 
 - (UIImage *)imageCroppedToRect:(CGRect)rect {
-	// CGImageCreateWithImageInRect's rect parameter is in pixels of the image's coordinates system. Convert to points.
+	// CGImageCreateWithImageInRect's `rect` parameter is in pixels of the image's coordinates system. Convert from points.
 	CGFloat scale = self.scale;
-	rect = CGRectMake(rect.origin.x *scale, rect.origin.y * scale, rect.size.width * scale, rect.size.height * scale);
+	rect = CGRectMake(rect.origin.x * scale, rect.origin.y * scale, rect.size.width * scale, rect.size.height * scale);
 	
-	CGImageRef imageRef = CGImageCreateWithImageInRect([self CGImage], rect);
+	CGImageRef imageRef = CGImageCreateWithImageInRect(self.CGImage, rect);
 	UIImage *cropped = [UIImage imageWithCGImage:imageRef];
 	CGImageRelease(imageRef);
 	return cropped;
