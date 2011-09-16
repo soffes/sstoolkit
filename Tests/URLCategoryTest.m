f9934f2e0f59b66a11bc4cfba647dbcacc4dcf08
Sam Soffes <sam@samsoff.es>
2011-09-16 05:54:54 -0700

    Added one more NSURL test for peace of mind

--- Tests/URLCategoryTest.m
+++ Tests/URLCategoryTest.m
@@ -14,8 +14,15 @@
 @implementation URLCategoryTest
 
 - (void)testURLWithFormat {
-	NSURL *url1 = [NSURL URLWithString:[NSString stringWithFormat:@"http://sstoolk.it/documentation/Categories/%@(SSToolkitAdditions).html", @"NSURL"]];
+	NSURL *url1 = [NSURL URLWithString:@"http://sstoolk.it/documentation/Categories/NSURL(SSToolkitAdditions).html"];
 	NSURL *url2 = [NSURL URLWithFormat:@"http://sstoolk.it/documentation/Categories/%@(SSToolkitAdditions).html", @"NSURL"];
+	
+	GHAssertNotNil(url2, nil);
+	GHAssertEqualObjects(url1, url2, nil);
+	
+	
+	url1 = [NSURL URLWithString:@"http://www.google.com/search?client=safari&rls=en&q=SSToolkit&ie=UTF-8&oe=UTF-8"];
+	url2 = [NSURL URLWithFormat:@"http://www.google.com/search?client=%@&rls=en&q=%@&ie=UTF-8&oe=UTF-8", @"safari", @"SSToolkit"];
 	
 	GHAssertNotNil(url2, nil);
 	GHAssertEqualObjects(url1, url2, nil);
