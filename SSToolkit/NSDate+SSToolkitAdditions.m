bcda900d7f8875ebdec5a324a76179b71a79fb07
Ryan Dorshorst <ryan@synth.tc>
2011-12-15 18:38:07 -0800

    Use NSDateFormatter for now while we consider threading issues with ISO8601 date formatter

--- SSToolkit/NSDate+SSToolkitAdditions.m
+++ SSToolkit/NSDate+SSToolkitAdditions.m
@@ -13,44 +13,23 @@
 @implementation NSDate (SSToolkitAdditions)
 
 + (NSDate *)dateFromISO8601String:(NSString *)iso8601 {
-	if (!iso8601) {
-		return nil;
+	static NSDateFormatter *utcDateFormatter = nil;
+	static NSDateFormatter *dateFormatter = nil;
+	
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		utcDateFormatter = [[NSDateFormatter alloc] init];
+		[utcDateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+		
+		dateFormatter = [[NSDateFormatter alloc] init];
+		[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZZ"];
+	});
+	
+	if ([iso8601 hasSuffix:@"Z"]) {
+		return [utcDateFormatter dateFromString:iso8601];
+	} else {
+		return [dateFormatter dateFromString:iso8601];
 	}
-
-	const char *str = [iso8601 cStringUsingEncoding:NSUTF8StringEncoding];
-	char newStr[24];
-
-	struct tm tm;
-	size_t len = strlen(str);
-	if (len == 0) {
-		return nil;
-	}
-
-	// UTC
-	if (len == 20 && str[len - 1] == 'Z') {
-		strncpy(newStr, str, len - 1);
-		strncpy(newStr + len - 1, "+0000\0", 6);
-	}
-
-	// Timezone
-	else if (len == 24 && str[22] == ':') {
-		strncpy(newStr, str, 22);    
-		strncpy(newStr + 22, str + 23, 2);
-	}
-
-	// Poorly formatted timezone
-	else {
-		strncpy(newStr, str, len > 24 ? 24 : len);
-	}
-
-	if (strptime(newStr, "%FT%T%z", &tm) == NULL) {
-		return nil;
-	}
-
-	time_t t; 
-	t = mktime(&tm);
-
-	return [NSDate dateWithTimeIntervalSince1970:t];
 }
 
 
