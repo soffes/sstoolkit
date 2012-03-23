a623af7918794c520d71b00f395bc2ccb00d9103
Sam Soffes <sam@samsoff.es>
2012-03-22 18:18:55 -0700

    Rename date categories

--- SSToolkit/NSDate+SSToolkitAdditions.m
+++ SSToolkit/NSDate+SSToolkitAdditions.m
@@ -71,7 +71,7 @@
 }
 
 
-- (NSString *)briefTimeAgoInWords {
+- (NSString *)briefTimeInWords {
 	NSTimeInterval seconds = fabs([self timeIntervalSinceNow]);
 	
 	// Seconds
@@ -123,7 +123,7 @@
 }
 
 
-+ (NSString *)timeAgoInWordsFromTimeInterval:(NSTimeInterval)intervalInSeconds includingSeconds:(BOOL)includeSeconds {
++ (NSString *)timeInWordsFromTimeInterval:(NSTimeInterval)intervalInSeconds includingSeconds:(BOOL)includeSeconds {
 	NSTimeInterval intervalInMinutes = round(intervalInSeconds / 60.0f);
 	
 	if (intervalInMinutes >= 0 && intervalInMinutes <= 1) {
@@ -166,13 +166,13 @@
 }
 
 
-- (NSString *)timeAgoInWords {
-	return [self timeAgoInWordsIncludingSeconds:YES];
+- (NSString *)timeInWords {
+	return [self timeInWordsIncludingSeconds:YES];
 }
 
 
-- (NSString *)timeAgoInWordsIncludingSeconds:(BOOL)includeSeconds {
-	return [[self class] timeAgoInWordsFromTimeInterval:fabs([self timeIntervalSinceNow]) includingSeconds:includeSeconds];		
+- (NSString *)timeInWordsIncludingSeconds:(BOOL)includeSeconds {
+	return [[self class] timeInWordsFromTimeInterval:fabs([self timeIntervalSinceNow]) includingSeconds:includeSeconds];		
 }
 
 
