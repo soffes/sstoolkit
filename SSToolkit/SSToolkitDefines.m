5621c186f89f38b494f243b85867e0f99230f274
Sam Soffes <sam@samsoff.es>
2012-04-24 22:49:09 -0700

    Fix resources

--- SSToolkit/SSToolkitDefines.m
+++ SSToolkit/SSToolkitDefines.m
@@ -6,7 +6,7 @@
 //  Copyright 2011 Sam Soffes. All rights reserved.
 //
 
-NSString *const kSSToolkitBundleName = @"SSToolkit.bundle";
+NSString *const kSSToolkitBundleName = @"SSToolkitResources.bundle";
 NSString *const kSSCollectionViewNilItemExceptionName = @"SSCollectionViewNilItemException";
 NSString *const kSSCollectionViewNilItemExceptionReason = @"SSCollectionView dataSource must return an item from collectionView:itemForIndexPath:";
 NSString *const SSCollectionViewInvalidItemSizeExceptionName = @"SSCollectionViewInvalidItemSizeException";
